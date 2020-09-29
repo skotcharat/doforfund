90d1f3ce2902bb6a1e0895fce2df16cfb32263b5
skotcharat <skotcharat@madisoncollege.edu>
2020-09-28 20:15:11 -0500

    week4 usedao

--- src/main/java/edu.matc.persistence/UserDao.java
+++ src/main/java/edu.matc.persistence/UserDao.java
@@ -1,10 +1,83 @@
 package edu.matc.persistence;
 
+import edu.matc.entity.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import javax.persistence.criteria.*;
+import java.util.List;
 
 public class UserDao {
     private final Logger logger = LogManager.getLogger(this.getClass());
     SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    /**
+     * Get user by id
+     */
+    public User getById(int id) {
+        Session session = sessionFactory.openSession();
+        User user = session.get( User.class, id );
+        session.close();
+        return user;
+    }
+
+    /**
+     * update user
+     * @param user  User to be inserted or updated
+     */
+    public void saveOrUpdate(User user) {
+        Session session = sessionFactory.openSession();
+        session.saveOrUpdate(user);
+        session.close();
+    }
+
+    /**
+     * update user
+     * @param user  User to be inserted or updated
+     */
+    public int insert(User user) {
+        int id = 0;
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        id = (int)session.save(user);
+        transaction.commit();
+        session.close();
+        return id;
+    }
+
+    /**
+     * Delete a user
+     * @param user User to be deleted
+     */
+    public void delete(User user) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(user);
+        transaction.commit();
+        session.close();
+    }
+
+    /** Return a list of all users
+     *
+     * @return All users
+     */
+    public List<User> getAll() {
+
+        Session session = sessionFactory.openSession();
+
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery( User.class );
+        Root<User> root = query.from( User.class );
+        List<User> users = session.createQuery( query ).getResultList();
+
+        logger.debug("The list of users " + users);
+        session.close();
+
+        return users;
+    }
+
+
 }
