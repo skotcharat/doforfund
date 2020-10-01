e0c58b9eb04ddfabb54756ba2670e73c61edf855
skotcharat <skotcharat@madisoncollege.edu>
2020-09-30 23:04:15 -0500

    week5 work

---
+++ src/main/java/edu.matc.entity/UserRole.java
@@ -0,0 +1,117 @@
+package edu.matc.entity;
+
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * The type Order.
+ */
+@Entity(name = "UserRole")  // type from class name
+@Table(name = "user_roles") // table name, case sensitive!
+public class UserRole { // @OneToMany
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    private String roleName;
+
+    /**
+     * Bidirectional @OneToMany
+
+     The bidirectional @OneToMany association also requires a @ManyToOne association on the child side.
+     Although the Domain Model exposes two sides to navigate this association, behind the scenes,
+     the relational database has only one foreign key for this relationship.
+
+     Every bidirectional association must have one owning side only (the child side),
+     the other one being referred to as the inverse (or the mappedBy) side.
+
+     Foreign key is on the child table (Order in this example)
+
+     By default, the @ManyToOne association assumes that the parent-side entity identifier is to be used to join
+     with the client-side entity Foreign Key column.
+
+     However, when using a non-Primary Key association,
+     the column description and foreign key should be used to instruct Hibernate
+     which column should be used on the parent side to establish the many-to-one database relationship.
+
+     Source: http://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#associations-one-to-many
+     */
+    @ManyToOne
+    private User user;
+
+    /**
+     * Instantiates a new Order.
+     */
+    public UserRole() {
+
+    }
+
+    /**
+     * Instantiates a new User role.
+     *
+     * @param roleName the role name
+     * @param user     the user
+     */
+    public UserRole(String roleName, User user) {
+        this.roleName = roleName;
+        this.user = user;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets role name.
+     *
+     * @return the role name
+     */
+    public String getRoleName() {
+        return roleName;
+    }
+
+    /**
+     * Sets role name.
+     *
+     * @param roleName the role name
+     */
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    /**
+     * Gets user.
+     *
+     * @return the user
+     */
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * Sets user.
+     *
+     * @param user the user
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
